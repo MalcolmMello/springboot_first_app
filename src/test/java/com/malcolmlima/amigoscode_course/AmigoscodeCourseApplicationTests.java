4b6a6187cae080508fecca98b950fd56635083ec
malcolm <malcolmmellolima@gmail.com>
2023-04-29 21:03:46 -0300

    primeira aplicação em springboot

---
+++ src/test/java/com/malcolmlima/amigoscode_course/AmigoscodeCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malcolmlima.amigoscode_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodeCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
